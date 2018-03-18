73e282561e7c588bfd31aa7c33fc40ae2992bb36
Sharad Shinde <shindesharad71@gmail.com>
2018-03-18 20:24:21 +0530

    more stuff in sw

--- sw.js
+++ sw.js
@@ -5,3 +5,16 @@ if('serviceWorker' in navigator) {
         console.log('Service worker registered!');
     });
 }
+
+self.addEventListener('install', function(event) {
+    console.log('Installing Service Worker!');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Activate Service Worker!');
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('Fetch Service Worker!');
+});
