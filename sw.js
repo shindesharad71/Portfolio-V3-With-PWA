00b096755d5feb903d4deee56ac27fa4b2452008
Sharad Shinde <shindesharad71@gmail.com>
2018-03-18 20:50:43 +0530

    PWA offline added

--- sw.js
+++ sw.js
@@ -7,6 +7,19 @@ if('serviceWorker' in navigator) {
 }
 
 self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('portfolio').then(function(cache) {
+          return cache.addAll(
+            [
+              '/style.css',
+              'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/css/materialize.min.css',
+              'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
+              'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
+              'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/js/materialize.min.js'
+            ]
+          );
+        })
+      );
     console.log('Installing Service Worker!');
 });
 
@@ -16,5 +29,15 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if(response) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    );
     console.log('Fetch Service Worker!');
 });
