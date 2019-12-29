546dc9a75a3afff9a6da45d15ee759213c2e8158
Sharad Shinde <shindesharad71@gmail.com>
2019-12-29 11:28:19 +0530

    Added: SW Formatting

--- sw.js
+++ sw.js
@@ -1,40 +1,35 @@
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-    .register('/sw.js')
-    .then(function() {
-        console.log('SW registered!');
-    });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(function() {
+    console.log("SW registered!");
+  });
 }
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('portfolio').then(function(cache) {
-          return cache.addAll(
-            [
-              '/style.css',
-              'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/css/materialize.min.css',
-              'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
-              'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
-              'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/js/materialize.min.js'
-            ]
-          );
-        })
-      );
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("portfolio").then(function(cache) {
+      return cache.addAll([
+        "/style.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/css/materialize.min.css",
+        "https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css",
+        "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/js/materialize.min.js"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-    return self.clients.claim();
+self.addEventListener("activate", function(event) {
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            if(response) {
-                return response;
-            } else {
-                return fetch(event.request);
-            }
-        })
-    );
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  );
 });
