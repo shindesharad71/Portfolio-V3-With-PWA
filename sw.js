df1ae0ae315b7dd9fc8fd0e1fd67a58b7cc0100a
SHARAD SHINDE <shindesharad71@gmail.com>
2020-09-28 18:49:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open("portfolio").then(function(cache) {
       return cache.addAll([
-        "/style.css",
         "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.95.3/css/materialize.min.css",
         "https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css",
         "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js",
